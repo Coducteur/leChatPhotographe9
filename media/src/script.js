a31df5ee1910fdc5dd5e33086a759c7ab3f271a5
Coducteur <chat.adr01@gmail.com>
2022-11-30 11:28:42 +0100

    adding JS in presentation.html

---
+++ media/src/script.js
@@ -0,0 +1,15 @@
+/* page presentation.html */
+
+/* display div with texts when click on first img or second img */
+const whoIAm = document.querySelector("#who-i-am");
+const onMouseOver = document.querySelector('.on-mouseOver')
+const matos = document.querySelector('#matos')
+const onMouseOver2 = document.querySelector('.on-mouseOver2')
+
+whoIAm.addEventListener("click", function () {
+    onMouseOver.classList.toggle("visible");
+});
+matos.addEventListener("click", function () {
+    onMouseOver2.classList.toggle("visible");
+});
+/* end of display div when click on first img or second img */
