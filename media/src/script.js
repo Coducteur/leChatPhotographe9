3ec33b8cbdb2e73af4542f53ac59d21338f636b7
Coducteur <chat.adr01@gmail.com>
2022-11-30 15:39:51 +0100

    adding function for toggle articles pres.html

--- media/src/script.js
+++ media/src/script.js
@@ -1,24 +1,58 @@
-/* page presentation.html */
+/* =====================================================
+common pages 
+ =====================================================*/
 
-/* display div with texts when click on first img or second img */
+/* =====================================================
+end of common pages 
+ =====================================================*/
+
+ /* =====================================================
+ page presentation.html 
+ =====================================================*/
+
+/* +++++++++++++++++++++++++++++++++++++++++++++++++++++
+display div with texts when click on first img or second img 
+++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+function toggleArticle() {
+    /*display div*/
+    onMouseOver.classList.toggle("visible");
+    /* erasing right content */
+    gridContainerMainArticle1.classList.toggle("invisible");
+    gridContainerMainArticle2.classList.toggle("invisible");
+    };
+/* declaring variables*/
+    /* img class click */
 const whoIAm = document.querySelector("#who-i-am");
-const onMouseOver = document.querySelector(".on-mouseOver");
 const matos = document.querySelector("#matos");
+    /*articles extent contents  */
+const onMouseOver = document.querySelector(".on-mouseOver");
 const onMouseOver2 = document.querySelector(".on-mouseOver2");
+    /* variables for show or hide extent content*/
 const gridContainerMainArticle1 = document.querySelector(
   ".gridContainer-mainArticle1"
 );
 const gridContainerMainArticle2 = document.querySelector(
   ".gridContainer-mainArticle2"
 );
+
+
+
+/* listening actions first article (click on img)*/
 whoIAm.addEventListener("click", function () {
-  /*display div*/
-  onMouseOver.classList.toggle("visible");
-  /* erasing right content */
-  gridContainerMainArticle1.remove();
-  gridContainerMainArticle2.remove();
+    toggleArticle();
 });
+/* listening actions second article (click on img)*/
 matos.addEventListener("click", function () {
   onMouseOver2.classList.toggle("visible");
 });
 /* end of display div when click on first img or second img */
+/*close div*/
+const btnCloseDiv = document.querySelector(".btn-close-div");
+btnCloseDiv.addEventListener("click", function () {
+    toggleArticle();
+});
+
+
+ /* =====================================================
+ end of page presentation.html 
+ =====================================================*/
