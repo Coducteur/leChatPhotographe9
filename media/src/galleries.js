69213f23c5c6056c5a5f4879f857e3f2ee6638eb
Coducteur <chat.adr01@gmail.com>
2022-12-01 16:13:56 +0100

    adding photos / + JS

---
+++ media/src/galleries.js
@@ -0,0 +1,63 @@
+// =====================================================
+// galleries pages 
+//  =====================================================*/
+
+// creating array "animals"
+const animals = [];
+
+for (let i = 1; i <= 28; i++) {
+  animals.push({
+    name: `animals(${i})`,
+    url: `./media/img_maxRez/animaux/animals(${i})`,
+  });
+};
+
+// creating array "cars"
+const cars = [];
+
+for (let i = 1; i <= 28; i++) {
+  cars.push({
+    name: `cars(${i})`,
+    url: `./media/img_maxRez/auto/cars(${i})`,
+  });
+};
+
+// creating array "nb"
+const nb = [];
+
+for (let i = 1; i <= 10; i++) {
+  nb.push({
+    name: `nb(${i})`,
+    url: `./media/img_maxRez/N&B/nb(${i})`,
+  });
+};
+
+// creating array "paysage"
+const paysage = [];
+
+for (let i = 1; i <= 7; i++) {
+    paysage.push({
+    name: `paysage(${i})`,
+    url: `./media/img_maxRez/paysage/paysage(${i})`,
+  });
+   
+};
+// creating array "cars"
+const spitzberg = [];
+
+for (let i = 1; i <= 17; i++) {
+    spitzberg.push({
+    name: `spitzberg(${i})`,
+    url: `./media/img_maxRez/spitzberg/spitzberg(${i})`,
+  });
+   
+};
+console.log(animals);
+console.log(cars);
+console.log(nb);
+console.log(paysage);
+console.log(spitzberg);
+
+/* =====================================================
+end of galleries pages 
+ =====================================================*/
